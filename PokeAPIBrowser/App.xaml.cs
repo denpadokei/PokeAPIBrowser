a115310cfcf21d893385c4e93c5011b688b95192
デンパ時計 <jsoldenpa@yahoo.co.jp>
2020-05-02 11:07:20 +0900

    プロジェクト ファイルを追加します。

---
+++ PokeAPIBrowser/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Prism.Ioc;
+using PokeAPIBrowser.Views;
+using System.Windows;
+using Prism.Modularity;
+using DenpadokeiFramework;
+using Prism.Regions;
+using PokeAPIBrowser.List;
+
+namespace PokeAPIBrowser
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            moduleCatalog.AddModule<ListModule>();
+            base.ConfigureModuleCatalog(moduleCatalog);
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
